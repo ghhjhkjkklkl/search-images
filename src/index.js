cf2703ec162b78bc405a673fc083a8bb0d062c62
ghhjhkjkklkl <99083436+ghhjhkjkklkl@users.noreply.github.com>
2023-04-09 11:16:31 +0200

    start#4

--- src/index.js
+++ src/index.js
@@ -1,126 +1,63 @@
-import { Notify } from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
+import PixabayService from './js/pixabay-api-service';
+import Notiflix from 'notiflix';
+import createCard from './js/create-card';
+import {
+  createGallerySimplelightbox,
+  refreshGallerySimplelightbox,
+} from './js/simple-light-box';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import PostApiService from './js/posts-service';
-import LoadMoreBtn from './js/load-more-btn';
-import PageLoadStatus from './js/load-status';
-import formSticky from './js/form-sticky';
 
-const refs = {
-  formSearch: document.querySelector('.search-form'),
-  gallery: document.querySelector('.gallery'),
-};
+const form = document.querySelector('#search-form');
+const galleryContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const pixabayService = new PixabayService();
 
-const loadMoreBtn = new LoadMoreBtn({
-  selector: '.load-more',
-  hidden: true,
-});
+form.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
-const pageLoadStatus = new PageLoadStatus({
-  selector: '.page-load-status',
-});
+function onSearch(evt) {
+  evt.preventDefault();
 
-const lightbox = new SimpleLightbox('.gallery__item', {
-  captionDelay: 250,
-  captionsData: 'alt',
-  enableKeyboard: true,
-});
-const obsOptions = {
-  root: null,
-  rootMargin: '100px',
-  treshold: 1,
-};
-const observer = new IntersectionObserver(onLoading, obsOptions);
+  pixabayService.query = evt.currentTarget.elements.searchQuery.value;
+  pixabayService.resetPage();
 
-const postApiService = new PostApiService();
+  if (pixabayService.query === '') {
+    return Notiflix.Notify.warning('Enter text to search the gallery.');
+  }
 
-refs.formSearch.addEventListener('submit', onSearch);
-loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
-window.addEventListener('scroll', formSticky);
-
-function onSearch(e) {
-  e.preventDefault();
-  pageLoadStatus.hide();
-  postApiService.query = e.target.searchQuery.value.trim();
-  loadMoreBtn.show();
-  postApiService.resetPage();
-  clearGallery();
-  fetchPosts();
-}
-
-function onLoadMore() {
-  fetchPosts();
-  pageLoadStatus.show();
-  smoothScrool();
-  observer.observe(pageLoadStatus.refs.pageLoadStatus);
-}
-
-function fetchPosts() {
-  loadMoreBtn.hide();
-  pageLoadStatus.loadingShow();
-
-  postApiService.fetchPost().then(data => {
-    const curentPage = postApiService.page - 1;
-    postApiService.hits = data.totalHits;
-
-    if (!data.totalHits) {
-      loadMoreBtn.hide();
-      pageLoadStatus.errorShow();
-
-      return Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-
-    if (!data.hits.length) {
-      loadMoreBtn.hide();
-      pageLoadStatus.lastElemShow();
-      return;
-    }
-    renderPost(data.hits);
-    if (curentPage === 1) {
-      Notify.success(`Hooray! We found ${postApiService.hits} images.`);
-      loadMoreBtn.show();
-    }
-    pageLoadStatus.enable();
+  clearGalleryContainer();
+  fetchImages().then(() => {
+    createGallerySimplelightbox();
   });
 }
 
-function renderPost(data) {
-  let markupPost = data
-    .map(
-      ({
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `<a class="gallery__item" href="${largeImageURL}">
-                  <div class="photo-card">
-                      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                      <div class="info">
-                        <p class="info-item"><b>Likes</b> ${likes}</p>
-                        <p class="info-item"><b>Views</b> ${views}</p>
-                        <p class="info-item"><b>Comments</b> ${comments}</p>
-                        <p class="info-item"><b>Downloads</b> ${downloads}</p>
-                      </div>
-                    </div>
-                 </a>`;
-      }
-    )
-    .join('');
-
-  refs.gallery.insertAdjacentHTML('beforeend', markupPost);
-  lightbox.refresh();
-}
-function clearGallery() {
-  refs.gallery.innerHTML = '';
+function onLoadMore() {
+  fetchImages().then(() => {
+    refreshGallerySimplelightbox();
+    smoothScroll();
+  });
 }
 
-function smoothScrool() {
+function fetchImages() {
+  loadMoreBtn.classList.add('is-hidden');
+
+  return pixabayService.fetchImages().then((images = []) => {
+    appendImagesMarkup(images);
+
+    const galleryItemsCount = document.querySelectorAll(
+      '.gallery .gallery-item'
+    ).length;
+
+    if (images.length === 0 || galleryItemsCount >= pixabayService.totalHits) {
+      loadMoreBtn.classList.add('is-hidden');
+    } else {
+      loadMoreBtn.classList.remove('is-hidden');
+    }
+  });
+}
+
+function smoothScroll() {
   const { height: cardHeight } = document
     .querySelector('.gallery')
     .firstElementChild.getBoundingClientRect();
@@ -131,11 +68,14 @@ function smoothScrool() {
   });
 }
 
-async function onLoading(entries) {
-  await entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      console.log(entry.isIntersecting);
-      fetchPosts();
-    }
+function clearGalleryContainer() {
+  galleryContainer.innerHTML = '';
+}
+
+function appendImagesMarkup(hits = []) {
+  if (!hits.length) return;
+
+  hits.forEach(hit => {
+    galleryContainer.insertAdjacentHTML('beforeend', createCard(hit));
   });
 }
